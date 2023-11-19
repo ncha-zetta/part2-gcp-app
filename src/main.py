b38770fc0b599479b146c0a256e486c78377902e
n cha <n.cha@example.com>
2023-11-20 00:12:32 +0200

    try connection unix socket

--- src/main.py
+++ src/main.py
@@ -7,13 +7,17 @@ def get_db_connection():
     password = os.environ.get('DB_PASSWORD')
     db_name = os.environ.get('DB_NAME')
 
-    return pymysql.connect(host=host, user=user, password=password, db=db_name)
+    unix_socket = f'/cloudsql/{host}'
+
+    return pymysql.connect(user=user, password=password, unix_socket=unix_socket, db=db_name)
 
 def list_vpcs_and_subnets(request):
     db_connection = get_db_connection()
     cursor = db_connection.cursor()
 
-    create_table_if_not_exists(cursor)
+    # create_table_if_not_exists(cursor)
+    # vpcs, subnets = query_gcp_vpcs_and_subnets()
+    # save_to_database(cursor, vpcs, subnets)
 
     cursor.close()
     db_connection.close()
