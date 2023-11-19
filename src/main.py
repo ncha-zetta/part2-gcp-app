693dc545b2f1dab61ad96f2daef7cb6cb2b7b5b8
n cha <n.cha@example.com>
2023-11-20 00:16:10 +0200

    implement create_table_if_not_exists and add schema

--- src/main.py
+++ src/main.py
@@ -11,11 +11,22 @@ def get_db_connection():
 
     return pymysql.connect(user=user, password=password, unix_socket=unix_socket, db=db_name)
 
+def create_table_if_not_exists(cursor):
+    create_table_query = """
+    CREATE TABLE IF NOT EXISTS network_info (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        resource_type VARCHAR(255),
+        name VARCHAR(255),
+        creation_timestamp DATETIME
+    )
+    """
+    cursor.execute(create_table_query)
+
 def list_vpcs_and_subnets(request):
     db_connection = get_db_connection()
     cursor = db_connection.cursor()
 
-    # create_table_if_not_exists(cursor)
+    create_table_if_not_exists(cursor)
     # vpcs, subnets = query_gcp_vpcs_and_subnets()
     # save_to_database(cursor, vpcs, subnets)
 
